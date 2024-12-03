394b95ffc33c188ade459689c016cb83c5dc6acd
Caleb Magruder <ccmagruder@gmail.com>
2024-12-03 03:36:27 +0000

    added : Matrix::iid factory

--- gemm/tests/bMatrix.cpp
+++ gemm/tests/bMatrix.cpp
@@ -4,16 +4,24 @@
 
 void bInitializeSquare(benchmark::State& state) {
     for (auto _ : state) {
-        Matrix A(state.range(0), state.range(0));
-        for (size_t i=0; i<state.range(0); i++) {
-            for (size_t j=0; j<state.range(0); j++) {
-                A[i][j] = 0;
-            }
-        }
+        size_t n = state.range(0);
+        Matrix A(n, n, 0);
     }
 }
 
 BENCHMARK(bInitializeSquare)
-    ->RangeMultiplier(2)->Range(1024, 8*1024)
+    ->RangeMultiplier(2)->Range(512, 2048)
     ->Unit(benchmark::kMillisecond);
 
+
+void bInitializeIID(benchmark::State& state) {
+
+    for (auto _ : state) {
+        size_t n = state.range(0);
+        std::unique_ptr<Matrix> A = Matrix::iid(n, n);
+    }
+}
+
+BENCHMARK(bInitializeIID)
+    ->RangeMultiplier(2)->Range(512, 2048)
+    ->Unit(benchmark::kMillisecond);
