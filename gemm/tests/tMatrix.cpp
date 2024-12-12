c2d7632a477d5a30863754b42bc04b1dad4f5088
Caleb Magruder <ccmagruder@users.noreply.github.com>
2024-12-12 10:16:16 -0600

    Adding Matrix Device Pointer (#10)

--- gemm/tests/tMatrix.cpp
+++ gemm/tests/tMatrix.cpp
@@ -2,8 +2,12 @@
 
 #include "Matrix.h"
 
-TEST(tMatrix, HelloWorld) {
-    Matrix A(10, 10);
-    EXPECT_TRUE(true);
+TEST(tMatrix, toFromHost) {
+    Matrix A(1, 1);
+    A[0][0] = 1;
+    A.toDevice();
+    A[0][0] = 2;
+    A.toHost();
+    EXPECT_EQ(A[0][0], 1);
 }
 
