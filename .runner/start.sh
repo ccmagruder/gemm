14b545b9c2e07835935d998813220d79492fcb05
Caleb Magruder <ccmagruder@gmail.com>
2024-12-14 21:35:37 +0000

    added : runner image inherits from nestybox/sysbox

--- .runner/start.sh
+++ .runner/start.sh
@@ -5,8 +5,9 @@ ACCESS_TOKEN=$(cat /run/secrets/token)
 
 REG_TOKEN=$(curl -X POST -H "Authorization: token ${ACCESS_TOKEN}" -H "Accept: application/vnd.github+json" https://api.github.com/repos/${REPO}/actions/runners/registration-token | jq .token --raw-output)
 
-cd /home/docker/actions-runner
+cd /home/runner/actions-runner
 
+export RUNNER_ALLOW_RUNASROOT=true
 ./config.sh --url https://github.com/${REPO} --token ${REG_TOKEN}
 
 cleanup() {
