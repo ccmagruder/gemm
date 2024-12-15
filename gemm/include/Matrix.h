285f8636c4a20c76b1c8d6b5b92759f9e51b2c9d
Caleb Magruder <ccmagruder@users.noreply.github.com>
2024-12-15 15:51:52 -0600

    Adding Matrix Factory Pattern (#14)

--- gemm/include/Matrix.h
+++ gemm/include/Matrix.h
@@ -4,50 +4,44 @@
 
 class Matrix {
  public:
-    static std::unique_ptr<Matrix> makeDevice(size_t m, size_t n) {
-        std::unique_ptr<Matrix> A = std::make_unique<Matrix>(m, n);
-        A->_devAlloc();
-        return A;
-    }
-
     Matrix(size_t m, size_t n)
-            : _m(m), _n(n), _host_ptr(new float[m*n]), _dev_ptr(nullptr) {}
+            : m(m), n(n), _host_ptr(new float[m*n]), _dev_ptr(nullptr) {}
 
-    Matrix(size_t m, size_t n, float value) : Matrix(m, n) {
-        for (size_t i=0; i<m*n; i++) {
-            this->_host_ptr[i] = value;
-        }
+    // Factories
+    static std::unique_ptr<Matrix> makeDevice(size_t m, size_t n);
+    static std::unique_ptr<Matrix> makeHost(size_t m, size_t n);
+
+    static std::unique_ptr<const Matrix> normalIID(size_t m, size_t n);
+    static std::unique_ptr<const Matrix> fill(size_t m, size_t n, float value);
+
+    // Accessors (Column Major)
+    const float& operator()(size_t i, size_t j) const {
+        return (this->_host_ptr.get() + this->m*j)[i];
+    }
+    float& operator()(size_t i, size_t j) {
+        return (this->_host_ptr.get() + this->m*j)[i];
     }
 
-    const float* const operator[](size_t i) const {
-        return this->_host_ptr.get() + this->_n*i;
-    }
-    float* operator[](size_t i) { return this->_host_ptr.get() + this->_n*i; }
-
-    const float* const get() const { return this->_host_ptr.get(); }
-    float* get() { return this->_host_ptr.get(); }
+    const float* const getHostPtr() const { return this->_host_ptr.get(); }
+    float* getHostPtr() { return this->_host_ptr.get(); }
 
     const float* const getDevPtr() const { return this->_dev_ptr.get(); }
     float* getDevPtr() { return this->_dev_ptr.get(); }
 
-    static std::unique_ptr<const Matrix> normalIID(size_t m, size_t n);
-
-    size_t m() const { return this->_m; }
-    size_t n() const { return this->_n; }
-
+    // Movers
     void toDevice() const;
     void toHost();
 
+ public:
+    const size_t m;
+    const size_t n;
  protected:
+    void _devAlloc() const;
+
+    std::unique_ptr<float[], std::default_delete<float[]>> _host_ptr;
     struct DevDeleter {
         void operator()(float* devPtr);
     };
-
-    void _devAlloc() const;
-
-    size_t _m;
-    size_t _n;
-    std::unique_ptr<float[], std::default_delete<float[]>> _host_ptr;
     mutable std::unique_ptr<float, DevDeleter> _dev_ptr;
 };
 
