723918494ad9b989cb3481370eb12b13b93b0133
Caleb Magruder <ccmagruder@gmail.com>
2024-12-15 22:12:22 +0000

    hotfix : ci/cd and local build systems

--- gemm/tests/tGemm.cpp
+++ gemm/tests/tGemm.cpp
@@ -4,12 +4,12 @@
 #include "Gemm.h"
 
 template <typename T>
-class tMatrixMult : public testing::Test {};
+class tGemm : public testing::Test {};
 
 using Types = ::testing::Types<Gemm, GemmCuBlas>;
-TYPED_TEST_SUITE(tMatrixMult, Types);
+TYPED_TEST_SUITE(tGemm, Types);
 
-TYPED_TEST(tMatrixMult, Ones) {
+TYPED_TEST(tGemm, Ones) {
     TypeParam mult(Matrix::fill(64, 32, 1), Matrix::fill(32, 16, 1));
     mult.compute();
     std::shared_ptr<Matrix> C = mult.get();
