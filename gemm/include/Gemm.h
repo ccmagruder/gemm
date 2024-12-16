f7e4e6c17237196ddf177603b6864c2e3ce6b8c4
Caleb Magruder <ccmagruder@gmail.com>
2024-12-16 21:53:36 +0000

    refactor : Gemm base class pure virtuals

--- gemm/include/Gemm.h
+++ gemm/include/Gemm.h
@@ -2,8 +2,6 @@
 
 #include <cassert>
 
-#include "cublas_v2.h"
-
 #include "Matrix.h"
 
 class Gemm {
@@ -11,11 +9,16 @@ class Gemm {
     Gemm(std::unique_ptr<const Matrix> A, std::unique_ptr<const Matrix> B)
       : _A(std::move(A)), _B(std::move(B)) {}
 
-    std::shared_ptr<Matrix> compute();
+    std::shared_ptr<Matrix> compute() {
+        this->_setup();
+        this->_run();
+        this->_teardown();
+        return this->get();
+    }
 
-    virtual void _setup();
-    virtual void _run();
-    virtual void _teardown();
+    virtual void _setup() = 0;
+    virtual void _run() = 0;
+    virtual void _teardown() = 0;
 
     std::shared_ptr<Matrix> get() { return this->_C; }
 
@@ -23,6 +26,15 @@ class Gemm {
     std::unique_ptr<const Matrix> _A;
     std::unique_ptr<const Matrix> _B;
     std::shared_ptr<Matrix> _C;
+};
+
+class GemmNaive : public Gemm {
+ public:
+    using Gemm::Gemm;
+
+    void _setup() override;
+    void _run() override;
+    void _teardown() override;
 };
 
 class GemmCuBlas : public Gemm {
