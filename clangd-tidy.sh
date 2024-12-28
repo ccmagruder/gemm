d2892808a9960aad70dbf03541571341cafae6f6
Jeffrey Hokanson (Baldr) <jeffreyh@hokanson.us>
2024-12-28 14:32:38 -0600

    Made a separte clangd-tidy script for CI

--- clangd-tidy.sh
+++ clangd-tidy.sh
@@ -1,20 +1,6 @@
 #! /bin/bash
-
-# Clangd by default will not run "expensive" checks. So we need two config
-# files: one for the language server; the other for the CI.
-# Because clangd-tidy does not support separate paths for config files,
-# we break these into separate files and do this moving hack.
-cp .clangd .clangd-orig
-echo "Diagnostics:" >> .clangd
-echo "  ClangTidy:" >> .clangd 
-echo "    FastCheckFilter: None" >> .clangd
-
-clangd-tidy -p build --fail-on-severity warn \
+clangd-tidy -p build \
     gemm/**/*.h \
     gemm/**/*.cpp \
     gemm/**/*.cuh \
     gemm/**/*.cu
-EXIT=$?
-
-mv .clangd-orig .clangd
-exit $EXIT
