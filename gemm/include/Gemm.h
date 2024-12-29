03c115fd16126747c5cc726af4698ebad928804f
Caleb Magruder <ccmagruder@gmail.com>
2024-12-29 12:42:38 -0600

    added : Gemm.hpp

--- gemm/include/Gemm.h
+++ gemm/include/Gemm.h
@@ -2,46 +2,7 @@
 
 #include <cassert>
 
-#include "Matrix.h"
-
-enum Memory { Host, Device };
-
-template <Memory T>
-class Gemm {
- public:
-  Gemm(std::unique_ptr<const Matrix> A, std::unique_ptr<const Matrix> B)
-      : _A(std::move(A)), _B(std::move(B)) {}
-
-  std::shared_ptr<Matrix> compute() {
-    this->_setup();
-    this->_run();
-    this->_teardown();
-    return this->get();
-  }
-
-  void _setup() { static_assert(false); }
-  virtual void _run() = 0;
-  void _teardown() { static_assert(false); };
-
-  std::shared_ptr<Matrix> get() { return this->_C; }
-
- protected:
-  std::unique_ptr<const Matrix> _A;
-  std::unique_ptr<const Matrix> _B;
-  std::shared_ptr<Matrix> _C;
-};
-
-template <>
-void Gemm<Host>::_setup();
-
-template <>
-void Gemm<Host>::_teardown();
-
-template <>
-void Gemm<Device>::_setup();
-
-template <>
-void Gemm<Device>::_teardown();
+#include "Gemm.hpp"
 
 class GemmNaive : public Gemm<Host> {
  public:
