98564843a6fd8583306f539dccedd57d756f72d8
Jeffrey M. Hokanson <jeffrey-hokanson@users.noreply.github.com>
2024-12-30 21:14:49 -0600

    Using clangd-tidy as a linter (#25)
    Co-authored-by: Jeffrey Hokanson (Baldr) <jeffreyh@hokanson.us>

---
+++ clang-format.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+# format the relivant files in place
+clang-format -i gemm/**/*.h gemm/**/*.cpp gemm/**/*.cuh gemm/**/*.cu
--- gemm/tests/bGemm.cpp
+++ gemm/tests/bGemm.cpp
@@ -1,4 +1,5 @@
 #include <benchmark/benchmark.h>
+#include <cstddef>
 
 #include "Gemm.h"
 #include "Matrix.h"
