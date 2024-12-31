05e0f8ef87bc7b08b7cb0c89680cdfbd7bd0c5da
Caleb Magruder <ccmagruder@gmail.com>
2024-12-30 22:56:22 -0600

    added : kernel comments

--- gemm/src/kernels.cu
+++ gemm/src/kernels.cu
@@ -23,28 +23,30 @@ __global__ void __sgemm(const int M,
     if (i >= M || j >= N)
         return;
 
-    const float* a = A + i;
-    const float* b = B + j * K;
-    float* const c = C + i + j * M;
+    const float* a = A + i;          // a = A[i, 0]
+    const float* b = B + j * K;      // b = B[0, j]
+    float* const c = C + i + j * M;  // c = C[i, j]
     *c = 0.0;
     for (int k = 0; k < K; k++) {
-        *c += *a * *b;
-        a += M;
-        b++;
+        *c += *a * *b;  // C[i, j] += A[i, k] * B[k, j]
+        a += M;         // A[i, k] -> A[i, k + 1]
+        b++;            // B[k, j] -> B[k + 1, j]
     }
 }
 
 void sgemm(const int M,
            const int N,
            const int K,
-           const float* const a,
-           const float* const b,
-           float* const c,
+           const float* const A,
+           const float* const B,
+           float* const C,
            const int V,
            const int W) {
-    dim3 gridDim(M / V + (M % V != 0), N / W + (N % W != 0), 1);
+    dim3 gridDim(M / V + (M % V != 0),  // gridDim.x = CEIL_DIV(M, V)
+                 N / W + (N % W != 0),  // gridDim.y = CEIL_DIV(N, W)
+                 1);                    // gridDim.z = 1
     dim3 blockDim(V, W, 1);
-    __sgemm<<<gridDim, blockDim>>>(M, N, K, a, b, c);
+    __sgemm<<<gridDim, blockDim>>>(M, N, K, A, B, C);
     cudaDeviceSynchronize();
     cudaCheck();
 }
