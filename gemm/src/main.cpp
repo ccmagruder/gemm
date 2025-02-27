dc20853ebd7381590d70c3f22d4eefbeffb7b5f5
Caleb Magruder <ccmagruder@gmail.com>
2025-02-26 21:57:05 -0600

    added : main executable test

--- gemm/src/main.cpp
+++ gemm/src/main.cpp
@@ -11,9 +11,7 @@ DEFINE_int32(N, 4096, "Cols of B, Cols of C");
 DEFINE_int32(blockDim_x, 32, "blockDim.x");
 DEFINE_int32(blockDim_y, 32, "blockDim.y");
 
-int main(int argc, char* argv[]) {
-    gflags::ParseCommandLineFlags(&argc, &argv, true);
-
+int timerGemmCuda(int M, int K, int N, int blockDim_x, int blockDim_y) {
     std::unique_ptr<const Matrix> A = Matrix::normalIID(FLAGS_M, FLAGS_K);
     std::unique_ptr<const Matrix> B = Matrix::normalIID(FLAGS_K, FLAGS_N);
 
@@ -23,20 +21,28 @@ int main(int argc, char* argv[]) {
     auto start = std::chrono::high_resolution_clock::now();
     mult.__run(FLAGS_blockDim_x, FLAGS_blockDim_y);
     auto end = std::chrono::high_resolution_clock::now();
-    auto duration =
-        std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
 
     mult._teardown();
-    std::shared_ptr<Matrix> Cref = mult.get();
+
+    return std::chrono::duration_cast<std::chrono::milliseconds>(end - start)
+        .count();
+}
+
+int main(int argc, char* argv[]) {
+    gflags::ParseCommandLineFlags(&argc, &argv, true);
+
+    int compute_time = timerGemmCuda(FLAGS_M, FLAGS_K, FLAGS_N,
+                                     FLAGS_blockDim_x, FLAGS_blockDim_y);
 
     std::cout << "{\n"
               << "    \"M\": " << FLAGS_M << ",\n"
               << "    \"K\": " << FLAGS_K << ",\n"
               << "    \"N\": " << FLAGS_N << ",\n"
-              << "    \"blockDim.x\": " << FLAGS_blockDim_x << ",\n"
-              << "    \"blockDim.y\": " << FLAGS_blockDim_y << ",\n"
-              << "    \"compute_time\": " << duration.count() << ",\n"
+              << "    \"blockDim_x\": " << FLAGS_blockDim_x << ",\n"
+              << "    \"blockDim_y\": " << FLAGS_blockDim_y << ",\n"
+              << "    \"compute_time\": " << compute_time << ",\n"
               << "    \"compute_time_units\": " << "\"ms\"" << ",\n"
               << "}\n";
+
     return 0;
 }
