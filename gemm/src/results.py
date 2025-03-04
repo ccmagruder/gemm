a447aac14ad782f613172cfc8f8fb42da6b4e592
Caleb Magruder <ccmagruder@gmail.com>
2025-03-03 23:20:51 -0600

    added : python class Results json load

--- gemm/src/results.py
+++ gemm/src/results.py
@@ -1,7 +1,30 @@
-import polars
+from typing import Any
+
+import json
+import polars as pl
+
 
 class Results:
-    def __init__(self, filename: str):
-        self.df = polars.read_json(filename)
-
-
+    def __init__(self, filename: str, *labels):
+        labels = list(labels)
+        with open(filename, "r") as file:
+            data = json.load(file)
+        self.context: dict[str, Any] = data["context"]
+        self.df = (
+            pl.from_dicts(
+                data["benchmarks"],
+                schema={
+                    "name": pl.String,
+                    "cpu_time": pl.Float64,
+                },
+            )
+            .with_columns(
+                pl.col("name")
+                .str.split_exact("/", 2 + len(labels))
+                .struct.rename_fields(["fixture", "benchmark", *labels])
+                .alias("fields")
+            )
+            .unnest("fields")
+            .cast({col: pl.Int64 for col in labels})
+            .drop("name")
+        )
--- gemm/tests/test_results.py
+++ gemm/tests/test_results.py
@@ -1,6 +1,7 @@
 from gemm import Results
 
+
 def test_results():
-    results = Results('bGemm.json')
+    results = Results("bGemm.json", "n")
     print(results.df)
     assert True
